bee79e5792db3915534ba15eb6b36ad80eb454a9
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-05-20 00:41:15 +0800

    Disable su binary version checking
    The checking seems doesn't work in KitKat.

--- Superuser/src/com/koushikdutta/superuser/util/SuHelper.java
+++ Superuser/src/com/koushikdutta/superuser/util/SuHelper.java
@@ -6,6 +6,7 @@ import android.util.Log;
 public class SuHelper {
     public static String CURRENT_VERSION = "16";
     public static void checkSu(Context context) throws Exception {
+/*
         Process p = Runtime.getRuntime().exec("su -v");
         String result = Settings.readToEnd(p.getInputStream());
         Log.i("Superuser", "Result: " + result);
@@ -19,5 +20,6 @@ public class SuHelper {
         String[] parts = result.split(" ");
         if (!CURRENT_VERSION.equals(parts[0]))
             throw new Exception("binary is old");
+*/
     }
 }
